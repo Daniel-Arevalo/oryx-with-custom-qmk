4913a5ae25449681ca2f3f84fc853221f33de75b
Daniel-Arevalo <100526518+Daniel-Arevalo@users.noreply.github.com>
2025-02-18 15:04:24 -0500

    Update keymap.c

--- xmZEY/keymap.c
+++ xmZEY/keymap.c
@@ -37,10 +37,10 @@ uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
     case LT(3,KC_SPACE):
     case LT(4,KC_BSPC):
     case LT(5,KC_SPACE):
-      return 0;  // Bypass Achordion for these keys.
+      return 100; 
   }
 
-  return 800;  // Otherwise use a timeout of 800 ms.
+  return 800; 
 }
 
 enum custom_keycodes {
